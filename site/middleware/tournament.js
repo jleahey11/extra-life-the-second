47992cb53c8c1aaf6ec8892c6f85a82fed4ac2db
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-10-05 10:49:17 -0400

    chore: commit floating work

--- site/middleware/tournament.js
+++ site/middleware/tournament.js
@@ -8,3 +8,31 @@ const actionAllowList = [
   TournamentActions.GET_TOURNAMENT_PARTICIPANTS
 ];
 
+const tournamentMiddleware = (store) => (next) => (action) => {
+  next(action);
+
+  if (!actionAllowList.includes(action.type)) return;
+
+  let request;
+
+  switch(action.type) {
+    case TournamentActions.GET_TOURNAMENT_PARTICIPANTS:
+      request = {
+        endpoint: `${window.location}api/challonge/${action.tournamentId}/participants`,
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        types: [
+          TournamentActions.FETCH_TOURNAMENT_PARTICIPANTS_REQUEST,
+          TournamentActions.FETCH_TOURNAMENT_PARTICIPANTS_SUCCESS,
+          TournamentActions.FETCH_TOURNAMENT_PARTICIPANTS_FAILURE
+        ]
+      };
+      break;
+    default:
+      return;
+  }
+
+  if (!!request) store.dispatch(createAction(request));
+}
+
+export default tournamentMiddleware;
